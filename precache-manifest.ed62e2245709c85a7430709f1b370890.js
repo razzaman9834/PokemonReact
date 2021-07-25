e3f03dd1ff900d270298c251fabc7468b3f93845
razzaman9834 <razzaman9834@gmail.com>
2021-07-25 17:16:20 +0530

    Updates

---
+++ precache-manifest.ed62e2245709c85a7430709f1b370890.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94db6a7fcd583a88a918c43819b57a2c",
+    "url": "/PokemonReact/index.html"
+  },
+  {
+    "revision": "845acd77d7abae040657",
+    "url": "/PokemonReact/static/css/main.72dce222.chunk.css"
+  },
+  {
+    "revision": "f1d8f551e64438b16078",
+    "url": "/PokemonReact/static/js/2.eca6e93d.chunk.js"
+  },
+  {
+    "revision": "845acd77d7abae040657",
+    "url": "/PokemonReact/static/js/main.47644181.chunk.js"
+  },
+  {
+    "revision": "7cdc9de79ed61904f61a",
+    "url": "/PokemonReact/static/js/runtime~main.7275af03.js"
+  },
+  {
+    "revision": "2f35105b5aa23e23810c6aefdbd6623f",
+    "url": "/PokemonReact/static/media/pattern.2f35105b.png"
+  }
+]);
